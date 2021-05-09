c0e120bd10acb928b4f7f1db18977c364d1886a3
Alex Chao <achao2018@gmail.com>
2021-05-09 10:33:27 -0500

    initial mood page

---
+++ src/pages/MoodPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ButtonAppBar from '../components/AppBar.js';
+
+export default function MoodPage() {
+
+  return (
+    <div>
+      <ButtonAppBar/>
+    </div>
+  );
+}
