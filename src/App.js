18dbeec1895f7cb8876b989083b489ad4ebf7bc0
jennychang0 <44443160+jennychang0@users.noreply.github.com>
2021-05-13 23:31:29 +0800

    first try

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import MoodPage from './pages/MoodPage.js';
+import React from 'react'
+
 
 function App() {
   return (
     <div>
       <MoodPage />
+
     </div>
   );
 }
--- src/pages/MoodPage.js
+++ src/pages/MoodPage.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ButtonAppBar from '../components/AppBar.js';
+import axios from 'axios';
+import { Credentials } from '../components/Credentials';
+import Dropdown from '../components/Dropdown';
 
 export default function MoodPage() {
+
+  const spotify = Credentials();  
+
+  const [token, setToken] = useState('');
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    axios('https://accounts.spotify.com/api/token', {
+      headers : {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa(spotify.CliendId + ':' + spotify.ClientSecret)
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {
+      console.log(tokenResponse.json());
+      console.log(tokenResponse.data.access_token);
+      setToken(tokenResponse.data.access_token);
+
+      axios('http://api.spotify.com/v1/browse/categories?local-sv_US', {
+        method: 'GET',
+        headers: {'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+      })
+      .then(genreResponse => {
+        setGenres(genreResponse.data.categories.items);
+        console.log(genres);
+      });
+
+    });
+
+  },[]);
 
   return (
     <div>
       <ButtonAppBar/>
+      {/* <Dropdown label="Genre :" options={genres} selectedValue={genres.selectedGenre} /> */}
       <h1>
         I have no idea what I'm doing :)
       </h1>
