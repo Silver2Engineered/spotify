0848a888a105b939338c43b4f7aa3ffd02ba9cda
Ariella Silver <a.silver@u.northwestern.edu>
2021-05-15 09:22:48 -0500

    authroization works

--- src/pages/MoodPage.js
+++ src/pages/MoodPage.js
@@ -3,40 +3,46 @@ import ButtonAppBar from '../components/AppBar.js';
 import axios from 'axios';
 import { Credentials } from '../components/Credentials';
 import Dropdown from '../components/Dropdown';
+var querystring = require('querystring');
 
 export default function MoodPage() {
 
-  const spotify = Credentials();  
+  const spotify = Credentials();
 
   const [token, setToken] = useState('');
   const [genres, setGenres] = useState([]);
 
   useEffect(() => {
-    axios('https://accounts.spotify.com/api/token', {
+    axios(`https://accounts.spotify.com/api/token`, {
       headers : {
         'Content-Type' : 'application/x-www-form-urlencoded',
-        'Authorization' : 'Basic ' + btoa(spotify.CliendId + ':' + spotify.ClientSecret)
+        'Authorization' : 'Basic ' + btoa(spotify.ClientId + ':' + spotify.ClientSecret)
       },
-      data: 'grant_type=client_credentials',
+      data: querystring.stringify ({
+        grant_type: "client_credentials",
+        redirect_uri: "http://localhost/3000",
+        code: spotify
+      }),
       method: 'POST'
     })
-    .then(tokenResponse => {
-      console.log(tokenResponse.json());
-      console.log(tokenResponse.data.access_token);
-      setToken(tokenResponse.data.access_token);
+    .then( 
+      (response) => { console.log(response)},
+      (error) => { console.log(error.response)}
+      //console.log(tokenResponse.json().body.key);
+      //console.log(tokenResponse.data.access_token);
+      //setToken(tokenResponse.data.access_token);
 
-      axios('http://api.spotify.com/v1/browse/categories?local-sv_US', {
-        method: 'GET',
-        headers: {'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
-      })
-      .then(genreResponse => {
-        setGenres(genreResponse.data.categories.items);
-        console.log(genres);
-      });
+    //   axios('http://api.spotify.com/v1/browse/categories?local-sv_US', {
+    //     method: 'GET',
+    //     headers: {'Authorization' : 'Bearer ' + tokenResponse.data.access_token}
+    //   })
+    //   .then(genreResponse => {
+    //     setGenres(genreResponse.data.categories.items);
+    //     console.log(genres);
+    //   });
 
-    });
-
-  },[]);
+    // });
+    )})
 
   return (
     <div>
