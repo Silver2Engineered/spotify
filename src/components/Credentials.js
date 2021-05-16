0d45deac76c8043f3701eeda318fe1ce9a8d310d
Molly Pribble <mollypribble1@mac.com>
2021-05-16 13:25:25 -0500

    oops forgot to push these files

---
+++ src/components/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: '5fae722c930b4f10bf50cd669ca2785f',
+        ClientSecret: 'ad39c8aa5a2d431e9ee6d824ba33451d'
+    }
+}
+
+export { Credentials };
